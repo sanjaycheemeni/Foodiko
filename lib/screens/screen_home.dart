1d0b404a1b19e1136ae0ce78eae3f332b4066cfa
Sanjaycheemeni <sanjaycheemeni@gmail.com>
2022-06-11 12:20:01 +0530

    SplashScreen Added

---
+++ lib/screens/screen_home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Center(child: Text('HomePage'))));
+  }
+}
---
+++ lib/screens/screen_splash.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:foodiko/screens/screen_home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3),(){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext bc)=>Homepage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: [
+          Text(
+            'Foodiko',
+            style: TextStyle(
+              fontSize: 45,
+              color: Color.fromARGB(255, 10, 39, 146),
+              fontWeight: FontWeight.bold
+            ),
+          ), 
+          Text('Canteen Token Management',
+          style: TextStyle(fontSize: 15,color: Color(0xFFAFAFAF)),)
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+    ));
+  }
+}
